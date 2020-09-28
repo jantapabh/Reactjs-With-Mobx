b0f623cdb0f62ecaf3ecc2bfd62fc4ceedf93ec8
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-28 20:52:36 +0700

    add commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
 import React from 'react';
-// import { useLocalStore,  observer  } from "mobx-react";
-// import { observer } from 'mobx-react';
-// import { observable } from "mobx"
 
-// const StoreContext = React.createContext();
-
-// const StoreProvider = ({ childern }) => {
-//   const store = useLocalStore(() => ({
-//     bugs: ["Centiped"]
-//   }));
-
-//   return (
-//     <StoreContext.Provider>{childern}</StoreContext.Provider>
-//   )
-
-// };
-
-// const BugsList = () => {
-//   const store = React.useContext(StoreContext);
-
-//   return(
-//     <ul>
-//       {
-//         store.bugs.map(bug => (
-//         <li key={bug}>{bug}</li>
-//         ))
-//       }
-//       </ul>
-//   );
-// };
-
-
-function App() {
+const App = () => {
   return (
-    // <StoreProvider>
-    //   <main><BugsList /></main>
-    //   </StoreProvider>
     <div>
       Test
       </div>
